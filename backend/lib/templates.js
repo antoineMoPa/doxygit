79876cb912b273a246ec0789eb573615155bdb59
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2014-11-09 18:06:05 -0500

    templates.js + move assets elsewhere

---
+++ backend/lib/templates.js
@@ -0,0 +1,19 @@
+var express  = require('express');
+var jade = require('jade');
+
+module.exports = function templates(){
+    return {
+        get: get
+    }
+    
+    function get(path){
+        var html = jade.renderFile(path);
+        return {
+            run: run,
+            html: html
+        }
+        function run(data){
+            return this.html;
+        }
+    }
+}
